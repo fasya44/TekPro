26ae7bd8635e26485bc99a73ef2731f6da294bef
FasyaNFauzziyah44 <78576654+fasya44@users.noreply.github.com>
2021-06-19 09:21:29 +0700

    Add files via upload

---
+++ W10/Bound.java
@@ -0,0 +1,30 @@
+package W10;
+
+public class Bound <T extends A> {
+	private T objRef; 
+	public Bound(T obj){ 
+		this.objRef = obj; 
+	} 
+	
+	public void doRunTest(){ 
+		this.objRef.displayClass(); 
+	} 
+} 
+	 
+class A { 
+	public void displayClass() { 
+		System.out.println("Inside super class A"); 
+	} 
+} 
+	 
+class B extends A { 
+	public void displayClass() { 
+		System.out.println("Inside sub class B"); 
+	} 
+} 
+	 
+class C extends A { 
+	public void displayClass() { 
+		System.out.println("Inside sub class C"); 
+	} 
+} 
